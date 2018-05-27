b8e46ea6db92fd6d4b5d7e621b9d9e935e99a675
fallenstardust <905309599@qq.com>
2018-05-27 20:43:34 +0800

    整理日志

--- mobile/src/main/java/cn/garymb/ygomobile/ui/home/HomeActivity.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/home/HomeActivity.java
@@ -1,8 +1,5 @@
 package cn.garymb.ygomobile.ui.home;
 
-import android.animation.Keyframe;
-import android.animation.ObjectAnimator;
-import android.animation.PropertyValuesHolder;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
@@ -63,10 +60,6 @@ abstract class HomeActivity extends BaseActivity implements NavigationView.OnNav
     protected SwipeMenuRecyclerView mServerList;
     private ServerListAdapter mServerListAdapter;
     private ServerListManager mServerListManager;
-
-    Button JoinQQGroup;
-
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -391,7 +384,6 @@ abstract class HomeActivity extends BaseActivity implements NavigationView.OnNav
         });
     }
     public boolean joinQQGroup(String key) {
-        //
         Intent intent = new Intent();
         intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D" + key));
         // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
