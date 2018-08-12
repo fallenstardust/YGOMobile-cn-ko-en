e7c1a5ae6f808c55b00b4d7453dfef2de741441f
purerosefallen <78877@qq.com>
2018-08-12 19:16:21 +0800

    update script reader

--- Classes/gframe/replay_mode.cpp
+++ Classes/gframe/replay_mode.cpp
@@ -65,13 +65,13 @@ int ReplayMode::ReplayThread(void* param) {
 	mainGame->dInfo.tag_player[1] = false;
 	if(mainGame->dInfo.isSingleMode) {
 #ifdef _IRR_ANDROID_PLATFORM_
-	set_script_reader(irr::android::android_script_reader);
+	set_script_reader((script_reader)SingleMode::ScriptReaderEx);
 #endif
 		set_card_reader((card_reader)DataManager::CardReader);
 		set_message_handler((message_handler)MessageHandler);
 	} else {
 #ifdef _IRR_ANDROID_PLATFORM_
-	set_script_reader(irr::android::android_script_reader);
+	set_script_reader((script_reader)ScriptReaderEx);
 #endif
 		set_card_reader((card_reader)DataManager::CardReader);
 		set_message_handler((message_handler)MessageHandler);
@@ -942,11 +942,11 @@ void ReplayMode::ReplayReload() {
 byte* ReplayMode::ScriptReaderEx(const char* script_name, int* slen) {
 	char sname[256] = "./expansions";
 	strcat(sname, script_name + 1);//default script name: ./script/c%d.lua
-	byte* buffer = default_script_reader(sname, slen);
+	byte* buffer = irr::android::android_script_reader(sname, slen);
 	if(buffer)
 		return buffer;
 	else
-		return default_script_reader(script_name, slen);
+		return irr::android::android_script_reader(script_name, slen);
 }
 int ReplayMode::MessageHandler(long fduel, int type) {
 	if(!enable_log)
