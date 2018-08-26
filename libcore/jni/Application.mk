66f2ada653c660b681a00eb799a2d52412d55642
Nanahira <78877@qq.com>
2018-08-26 20:23:20 +0800

    more changes

--- libcore/jni/Application.mk
+++ libcore/jni/Application.mk
@@ -4,6 +4,7 @@ APP_PLATFORM := android-14
 #APP_MODULES := YGOMobile
 #NDK_TOOLCHAIN_VERSION=4.8
 APP_ALLOW_MISSING_DEPS=true
-APP_STL := gnustl_static
+#APP_STL := gnustl_static
+APP_STL := c++_static
 APP_CPPFLAGS := -Wno-error=format-security -std=gnu++11 -fpermissive -D__cplusplus=201103L
 APP_OPTIM := release
