d340d812007614e6603de2f6d2ddca2dceac34bc
fallenstardust <905309599@qq.com>
2018-10-22 15:20:36 +0800

    发现新版本

--- mobile/src/main/java/cn/garymb/ygomobile/ui/home/HomeActivity.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/home/HomeActivity.java
@@ -412,7 +412,7 @@ public abstract class HomeActivity extends BaseActivity implements NavigationVie
                         final String versionName,updateMessage;
                         versionName = appBean.getVersionName();
                         updateMessage = appBean.getReleaseNote();
-                        builder.setTitle(R.string.Update_Found);
+                        builder.setTitle(context.getResources().getString(R.string.Update_Found) + versionName);
                         builder.setMessage(updateMessage);
                         builder.setRightButtonText(R.string.Download);
                         builder.setRightButtonListener((dlg, i) -> {
