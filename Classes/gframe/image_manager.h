f6e08f320003f8d6fa646029d175007dc0f79403
fallenstardust <905309599@qq.com>
2019-03-26 11:16:34 +0800

    add image

--- Classes/gframe/image_manager.h
+++ Classes/gframe/image_manager.h
@@ -31,6 +31,7 @@ public:
 	irr::video::ITexture* tUnknown;
 	irr::video::ITexture* tAct;
 	irr::video::ITexture* tAttack;
+	irr::video::ITexture* tTotalAtk;
 	irr::video::ITexture* tNegated;
 	irr::video::ITexture* tChain;
 	irr::video::ITexture* tNumber;
