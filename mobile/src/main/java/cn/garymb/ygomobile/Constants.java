525fcd482a67c245e2901cd870c7b11f5125e08e
fallenstardust <905309599@qq.com>
2019-06-16 13:47:58 +0800

    3.4.5LOGO 回滚删除确认 长按监听事件+0.2s

--- mobile/src/main/java/cn/garymb/ygomobile/Constants.java
+++ mobile/src/main/java/cn/garymb/ygomobile/Constants.java
@@ -152,7 +152,7 @@ public interface Constants {
     /***
      * 长按删除
      */
-    long LONG_PRESS_DRAG = 700;
+    long LONG_PRESS_DRAG = 900;
     /***
      * adb shell am start -n cn.garymb.ygomobile/cn.garymb.ygomobile.ui.home.MainActivity -a ygomobile.intent.action.DECK --es android.intent.extra.TEXT 青眼白龙.ydk
      * <p>
