826024bb82a50f879fcf5c26edea85b83f0592b2
fallenstardust <905309599@qq.com>
2019-07-19 13:40:37 +0800

    windbot搬到ygocore下

--- mobile/src/main/java/cn/garymb/ygomobile/ui/home/MainActivity.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/home/MainActivity.java
@@ -229,6 +229,9 @@ public class MainActivity extends HomeActivity {
                 IOUtils.copyFilesFromAssets(this, getDatapath(Constants.CORE_STRING_PATH),
                         AppsSettings.get().getResourcePath(), true);
 
+                IOUtils.copyFilesFromAssets(this, getDatapath(Constants.WINDBOT_PATH),
+                        AppsSettings.get().getResourcePath(), true);
+
                 IOUtils.copyFilesFromAssets(this, getDatapath(Constants.CORE_SKIN_PATH),
                         AppsSettings.get().getCoreSkinPath(), false);
             } catch (IOException e) {
