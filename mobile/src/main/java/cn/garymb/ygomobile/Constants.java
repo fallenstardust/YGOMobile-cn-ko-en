6154307273d0a6fe18d41dd54343d9c2b49d0cab
fallenstardust <905309599@qq.com>
2019-09-15 22:25:23 +0800

    新教程地址

--- mobile/src/main/java/cn/garymb/ygomobile/Constants.java
+++ mobile/src/main/java/cn/garymb/ygomobile/Constants.java
@@ -141,7 +141,7 @@ public interface Constants {
     int DECK_EXTRA_COUNT = (DECK_SIDE_MAX / DECK_WIDTH_COUNT * DECK_WIDTH_COUNT < DECK_SIDE_MAX) ? DECK_WIDTH_COUNT * 2 : DECK_WIDTH_COUNT;
     int DECK_SIDE_COUNT = DECK_EXTRA_COUNT;
     String ALIPAY_URL = "HTTPS://QR.ALIPAY.COM/FKX06491UAXJMGIDTYVC0C";
-    String URL_HELP = "http://www.jianshu.com/p/a43f5d951a25";
+    String URL_HELP = "http://note.youdao.com/noteshare?id=8ae2dc824b7dc04a95a4665a938e2251";
     String URL_MASTERRULE_CN = "https://ocg-rule.readthedocs.io/zh_CN/master/";
     String WIKI_SEARCH_URL = "https://www.ourocg.cn/S.aspx?key=";
 
