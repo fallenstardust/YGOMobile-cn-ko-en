1c80eeba2856bf083e230f3ae8a6583c8671fd2b
fallenstardust <905309599@qq.com>
2019-10-22 13:11:19 +0800

    tweak

--- mobile/src/main/java/cn/garymb/ygomobile/ui/preference/YGOPreferencesProvider.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/preference/YGOPreferencesProvider.java
@@ -5,6 +5,6 @@ import com.zlm.libs.preferences.PreferencesProvider;
 public class YGOPreferencesProvider extends PreferencesProvider {
     @Override
     public String getAuthorities() {
-        return "cn.garymb.ygomobile.ui.preference.YGOPreferencesProvider";
+        return cn.garymb.ygomobile.lite.BuildConfig.APPLICATION_ID + ".preference";
     }
 }
