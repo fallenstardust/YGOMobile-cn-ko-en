81e6db1497c746e92378d64498030737b594f710
kenan <247321453@qq.com>
2019-10-22 14:31:33 +0800

    opt mk

--- libcore/jni/Application.mk
+++ libcore/jni/Application.mk
@@ -1,10 +1,6 @@
 APP_ABI := armeabi-v7a
-#APP_ABI := armeabi-v7a 
-APP_PLATFORM := android-14
+APP_PLATFORM := android-19
 #APP_MODULES := YGOMobile
-#NDK_TOOLCHAIN_VERSION=4.8
-APP_ALLOW_MISSING_DEPS=true
-#APP_STL := gnustl_static
 APP_STL := c++_static
 APP_CPPFLAGS := -Wno-error=format-security -std=gnu++11 -fpermissive -D__cplusplus=201103L
 APP_OPTIM := release
