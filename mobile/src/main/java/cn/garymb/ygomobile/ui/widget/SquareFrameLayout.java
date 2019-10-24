8f3846e7e7a35957d86c3536265575e27ce91336
kenan <247321453@qq.com>
2019-10-24 17:42:41 +0800

    logo

---
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/widget/SquareFrameLayout.java
@@ -0,0 +1,27 @@
+package cn.garymb.ygomobile.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class SquareFrameLayout extends FrameLayout {
+    public SquareFrameLayout(@NonNull Context context) {
+        super(context);
+    }
+
+    public SquareFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
