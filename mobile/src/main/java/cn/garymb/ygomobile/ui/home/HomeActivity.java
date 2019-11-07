df5f0ffef15729d5a68ab8ef90375d72efaf76d1
fallenstardust <905309599@qq.com>
2019-11-07 20:53:35 +0800

    update strings.conf 自带浏览器intent

--- mobile/src/main/java/cn/garymb/ygomobile/ui/home/HomeActivity.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/home/HomeActivity.java
@@ -337,9 +337,7 @@ public abstract class HomeActivity extends BaseActivity implements NavigationVie
                     dialog.dismiss();
                 });
                 btnpaypal.setOnClickListener((v) -> {
-                    Uri uri = Uri.parse("https://www.paypal.me/YGOmobile3");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
+                    WebActivity.open(this, getString(R.string.donation), Constants.PAYPAL_URL);
                     dialog.dismiss();
                 });
             }
