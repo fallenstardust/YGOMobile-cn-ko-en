fe40188f862cd50ea9d42331dfba4ddbbef635cf
fallenstardust <905309599@qq.com>
2019-12-03 14:47:34 +0800

    由本身来支持x86

--- libcore/jni/Application.mk
+++ libcore/jni/Application.mk
@@ -1,5 +1,5 @@
-APP_ABI := armeabi-v7a
-#APP_ABI := armeabi-v7a 
+APP_ABI := armeabi-v7a x86
+#APP_ABI := armeabi-v7a x86
 APP_PLATFORM := android-14
 #APP_MODULES := YGOMobile
 #NDK_TOOLCHAIN_VERSION=4.8
