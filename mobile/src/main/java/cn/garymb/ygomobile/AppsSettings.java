f79deedb72d7128287feda30384ad72e548cac10
kenan <247321453@qq.com>
2021-09-20 22:16:15 +0800

    getSystemConfig

--- mobile/src/main/java/cn/garymb/ygomobile/AppsSettings.java
+++ mobile/src/main/java/cn/garymb/ygomobile/AppsSettings.java
@@ -677,8 +677,9 @@ public class AppsSettings {
         mSharedPreferences.putString(Constants.PREF_LAST_ROOM_LIST, array.toString());
     }
 
+    @Deprecated
     //获取收藏文件
-    public String getFavoriteTxt() {
-        return new File(getResourcePath(), "/favorite.txt").getAbsolutePath();
+    public File getFavoriteFile() {
+        return new File(getResourcePath(), "/favorite.txt");
     }
 }
