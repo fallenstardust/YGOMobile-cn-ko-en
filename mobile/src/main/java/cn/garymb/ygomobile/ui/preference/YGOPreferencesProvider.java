778a81cb88ba5aa98a9ff2b6327eb2c511c5e438
qq247321453 <247321453@qq.com>
2021-09-26 16:43:04 +0800

    getAuthorities

--- mobile/src/main/java/cn/garymb/ygomobile/ui/preference/YGOPreferencesProvider.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/preference/YGOPreferencesProvider.java
@@ -2,9 +2,11 @@ package cn.garymb.ygomobile.ui.preference;
 
 import com.zlm.libs.preferences.PreferencesProvider;
 
+import cn.garymb.ygomobile.App;
+
 public class YGOPreferencesProvider extends PreferencesProvider {
     @Override
     public String getAuthorities() {
-        return "cn.garymb.ygomobile.ui.preference.YGOPreferencesProvider";
+        return App.get().getPackageName() + ".ui.preference.YGOPreferencesProvider";
     }
 }
