09eb188d8964dc7f3f1bb8325430e79ec659bdd4
noname <noname@qq.com>
2021-10-03 09:03:39 +0800

    joinGame自动退出

--- libcore/jni/cn_garymb_ygomobile_core_IrrlichtBridge.cpp
+++ libcore/jni/cn_garymb_ygomobile_core_IrrlichtBridge.cpp
@@ -186,6 +186,7 @@ static void* join_game_thread(void* param) {
 		ygo::mainGame->gMutex.unlock();
 		return NULL;
 	}
+	exit_on_return = true;
 	irr::android::YGOGameOptions options = irr::android::YGOGameOptions(param);
 	irr::SEvent event;
 
