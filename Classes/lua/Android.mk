d5e1d98c443df53fd7250e26a4a7e58b464fe094
mercury233 <me@mercury233.me>
2021-12-13 09:49:46 +0800

    Revert "update lua5.4"
    This reverts commit 33889f85dbdd2aa6eef6f1ba0c0620dbce1c9978.

--- Classes/lua/Android.mk
+++ Classes/lua/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := lua5.4
+LOCAL_MODULE    := lua5.3
 LOCAL_SRC_FILES := lapi.c \
                    lauxlib.c \
                    lbaselib.c \
