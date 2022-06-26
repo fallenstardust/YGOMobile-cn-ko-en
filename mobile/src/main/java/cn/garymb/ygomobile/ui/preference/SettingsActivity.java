cc84830b6b5f63c1242e22c280f8d1120eaaa0b1
fallenstardust <905309599@qq.com>
2022-06-26 21:34:47 +0800

    微调布局

--- mobile/src/main/java/cn/garymb/ygomobile/ui/preference/SettingsActivity.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/preference/SettingsActivity.java
@@ -12,7 +12,7 @@ public class SettingsActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_settings);
+        setContentView(R.layout.activity_settings);
         getSupportActionBar().hide();
         enableBackHome();
         getFragmentManager().beginTransaction().replace(R.id.fragment, new SettingFragment()).commit();
