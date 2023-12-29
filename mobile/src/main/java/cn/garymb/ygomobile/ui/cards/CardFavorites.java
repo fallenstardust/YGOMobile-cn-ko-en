8ad39a5d8bd92a4b18c683837a6a981c9264400b
fallenstardust <darkness671884>
2023-12-29 15:45:42 +0800

    为收藏夹转换先行id为正式id 将先行对照表迁移至comparisonTableUtil

--- mobile/src/main/java/cn/garymb/ygomobile/ui/cards/CardFavorites.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/cards/CardFavorites.java
@@ -1,5 +1,10 @@
 package cn.garymb.ygomobile.ui.cards;
 
+import static cn.garymb.ygomobile.ui.home.HomeActivity.pre_code_list;
+import static cn.garymb.ygomobile.ui.home.HomeActivity.released_code_list;
+import static cn.garymb.ygomobile.utils.ComparisonTableUtil.newIDsArray;
+import static cn.garymb.ygomobile.utils.ComparisonTableUtil.oldIDsArray;
+
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.SparseArray;
@@ -12,6 +17,7 @@ import cn.garymb.ygomobile.AppsSettings;
 import cn.garymb.ygomobile.Constants;
 import cn.garymb.ygomobile.loader.ICardSearcher;
 import cn.garymb.ygomobile.utils.FileUtils;
+import cn.hutool.core.util.ArrayUtil;
 import ocgcore.data.Card;
 
 /**
@@ -92,7 +98,14 @@ public class CardFavorites {
         for (String line : lines) {
             String tmp = line.trim();
             if (!tmp.isEmpty() && TextUtils.isDigitsOnly(tmp)) {
-                mList.add(Integer.parseInt(tmp));
+                Integer id = Integer.parseInt(tmp);
+                if (released_code_list.contains(id)) {//先查看id对应的卡片密码是否在正式数组中存在
+                    id = pre_code_list.get(released_code_list.indexOf(id));//替换成对应先行数组里的code
+                }//执行完后变成先行密码，如果constants对照表里存在该密码，则如下又转换一次，所以发布app后必须及时更新在线对照表
+                if (ArrayUtil.contains(oldIDsArray, id)) {
+                    id = ArrayUtil.get(newIDsArray, ArrayUtil.indexOf(oldIDsArray, id));
+                }
+                mList.add(id);
             }
         }
         Log.d(TAG, "load favorites success:" + mList.size());
