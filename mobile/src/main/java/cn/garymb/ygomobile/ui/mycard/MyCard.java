d10783a56d3687f6d56395ca341b53cb80f3942d
fallenstardust <darkness671884>
2024-09-20 17:23:58 +0800

    更改萌卡接口地址 移除停用的接口

--- mobile/src/main/java/cn/garymb/ygomobile/ui/mycard/MyCard.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/mycard/MyCard.java
@@ -46,61 +46,28 @@ import okhttp3.Response;
 
 public class MyCard {
 
-    private static final String mHomeUrl = "https://mycard.moe/mobile/";
-    private static final String mArenaUrl = "https://mycard.moe/ygopro/arena/";
-    public static final String mCommunityIndexUrl = "https://ygobbs.com/";
-    public static final String mCommunityReportUrl = "https://ygobbs.com/t/bug%E5%8F%8D%E9%A6%88/";
-    private static final String mCommunityUrl = "https://ygobbs.com/login";
-    public static final String mCompetitionUrl = "https://event.ygobbs.com/";
-    private static final String return_sso_url = "https://mycard.moe/mobile/?";
-    private static final String HOST_MC = "mycard.moe";
-    public static final String MC_MAIN_URL = "https://mycard.moe/mobile/ygopro/lobby";
+    private static final String mHomeUrl = "https://mycard.world/mobile/";
+    private static final String mArenaUrl = "https://mycard.world/ygopro/arena/";
+    public static final String mCommunityReportUrl = "https://ygobbs2.com/t/bug%E5%8F%8D%E9%A6%88/";
+    private static final String mCommunityUrl = "https://ygobbs2.com/login";
+    public static final String mCompetitionUrl = "https://event.ygobbs2.com/";
+    private static final String HOST_MC = "mycard.world";
+    public static final String MC_MAIN_URL = "https://mycard.world/mobile/ygopro/lobby";
 
-    public static final String MYCARD_NEWS_URL = "https://api.mycard.moe/apps.json";
-    public static final String MYCARD_POST_URL = "https://ygobbs.com/t/";
-    public static final String YGO_LFLIST_URL = "https://raw.githubusercontent.com/moecube/ygopro/server/lflist.conf";
-
-    public static final String ARG_TOPIC_LIST = "topic_list";
-    public static final String ARG_TOPICS = "topics";
+    public static final String MYCARD_NEWS_URL = "https://sapi.moecube.com:444/apps.json";
+    public static final String MYCARD_POST_URL = "https://ygobbs2.com/t/";
     public static final String ARG_ID = "id";
     public static final String ARG_TITLE = "title";
-    public static final String ARG_IMAGE_URL = "image_url";
-    public static final String ARG_CREATE_TIME = "created_at";
-    public static final String ARG_OTHER = "other";
 
 
     public static final String ARG_MC_NAME = "name";
-    public static final String ARG_MC_PASSWORD = "password";
     public static final String ARG_YGOPRO = "ygopro";
     public static final String ARG_ZH_CN = "zh-CN";
     public static final String ARG_IMAGE = "image";
     public static final String ARG_UPDATE_AT = "updated_at";
     public static final String ARG_URL = "url";
     public static final String ARG_NEWS = "news";
-    public static final String ARG_USERNAME = "username";
-    public static final String MYCARD_USER_DUEL_URL = "https://sapi.moecube.com:444/ygopro/arena/user";
-
-    public static final String ACTION_OPEN_MYCARD = "ygomobile.intent.action.MYCARD";
-    public static final String URL_MC_LOGIN = "https://accounts.moecube.com/";
     public static final String URL_MC_LOGOUT = "https://accounts.moecube.com/signin";
-    public static final String ARG_SSO = "sso";
-    public static final String URL_MC_WATCH_DUEL_FUN = "wss://tiramisu.mycard.moe:7923/?filter=started";
-    public static final String URL_MC_WATCH_DUEL_MATCH = "wss://tiramisu.mycard.moe:8923/?filter=started";
-    public static final String URL_MC_MATCH = "https://api.mycard.moe/ygopro/match";
-    public static final String ARG_EVENT = "event";
-    public static final String ARG_DATA = "data";
-    public static final String HOST_MC_MATCH = "tiramisu.mycard.moe";
-    public static final String HOST_MC_OTHER = "tiramisu.mycard.moe";
-    public static final int PORT_MC_MATCH = 8911;
-    public static final int PORT_MC_OTHER = 7911;
-    public static final String ARG_LOCALE = "locale";
-    public static final String ARG_ARENA = "arena";
-    public static final String ARG_ATHLEIC = "athletic";
-    public static final String ARG_ENTERTAIN = "entertain";
-    public static final String ARG_ADDRESS = "address";
-    public static final String ARG_PORT = "port";
-    public static final String PACKAGE_NAME_EZ = "com.ourygo.ez";
-    private static final Charset UTF_8 = StandardCharsets.UTF_8;
     private final DefWebViewClient mDefWebViewClient = new DefWebViewClient() {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
