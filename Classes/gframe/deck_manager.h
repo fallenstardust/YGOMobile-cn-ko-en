8e0fdfe16d105d5eb3df1f91e03cdd9b9867df16
fallenstardust <905309599@qq.com>
2025-05-18 13:55:09 +0800

    update gframe

--- Classes/gframe/deck_manager.h
+++ Classes/gframe/deck_manager.h
@@ -36,6 +36,12 @@ struct Deck {
 	}
 };
 
+struct DeckArray {
+	std::vector<uint32_t> main;
+	std::vector<uint32_t> extra;
+	std::vector<uint32_t> side;
+};
+
 class DeckManager {
 public:
 	Deck current_deck;
@@ -50,7 +56,6 @@ public:
 	unsigned int CheckDeck(const Deck& deck, unsigned int lfhash, int rule);
 	bool LoadCurrentDeck(const wchar_t* file, bool is_packlist = false);
 	bool LoadCurrentDeck(irr::gui::IGUIComboBox* cbCategory, irr::gui::IGUIComboBox* cbDeck);
-	bool SaveDeckBuffer(const int deckbuf[], const wchar_t* name);
 
 	static uint32_t LoadDeck(Deck& deck, uint32_t dbuf[], int mainc, int sidec, bool is_packlist = false);
 	static uint32_t LoadDeckFromStream(Deck& deck, std::istringstream& deckStream, bool is_packlist = false);
@@ -64,6 +69,7 @@ public:
 	static bool CreateCategory(const wchar_t* name);
 	static bool RenameCategory(const wchar_t* oldname, const wchar_t* newname);
 	static bool DeleteCategory(const wchar_t* name);
+	static bool SaveDeckArray(const DeckArray& deck, const wchar_t* name);
 	
 	int TypeCount(std::vector<code_pointer> list, unsigned int ctype);
 };
