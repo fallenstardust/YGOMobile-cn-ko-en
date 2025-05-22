700f263c4331a2fd515ccb3c2d0be2989903fd99
fallenstardust <darkness671884>
2025-05-22 17:11:33 +0800

    update gframe

--- Classes/gframe/menu_handler.h
+++ Classes/gframe/menu_handler.h
@@ -2,6 +2,8 @@
 #define MENU_HANDLER_H
 
 #include <irrlicht.h>
+#include "replay.h"
+
 #ifdef _IRR_ANDROID_PLATFORM_
 #include <android/TouchEventTransferAndroid.h>
 #endif
@@ -13,7 +15,7 @@ public:
 	bool OnEvent(const irr::SEvent& event) override;
 	irr::s32 prev_operation{ 0 };
 	int prev_sel{ -1 };
-
+	Replay temp_replay;
 };
 
 }
