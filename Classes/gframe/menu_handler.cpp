406d666c5d0c8b2d57b28361433942ce471e7603
jwyxym <jwyxym@126.com>
2025-08-12 17:36:13 +0800

    Update menu_handler.cpp

--- Classes/gframe/menu_handler.cpp
+++ Classes/gframe/menu_handler.cpp
@@ -169,6 +169,48 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 					mainGame->btnJoinHost->setEnabled(false);
 					mainGame->btnJoinCancel->setEnabled(false);
 				}
+				#else
+				char ip[20];
+				wchar_t pstr[100];
+				wchar_t portstr[10];
+				BufferIO::CopyWideString(mainGame->ebJoinHost->getText(), pstr);
+				BufferIO::CopyWideString(mainGame->ebJoinPort->getText(), portstr);
+				BufferIO::EncodeUTF8(pstr, ip);
+				unsigned int remote_addr = htonl(inet_addr(ip));
+				if(remote_addr == -1) {
+					char hostname[100];
+					char port[6];
+					BufferIO::EncodeUTF8(pstr, hostname);
+					BufferIO::EncodeUTF8(portstr, port);
+					struct evutil_addrinfo hints;
+					struct evutil_addrinfo *answer = nullptr;
+					std::memset(&hints, 0, sizeof hints);
+					hints.ai_family = AF_INET;
+					hints.ai_socktype = SOCK_STREAM;
+					hints.ai_protocol = IPPROTO_TCP;
+					hints.ai_flags = EVUTIL_AI_ADDRCONFIG;
+					int status = evutil_getaddrinfo(hostname, port, &hints, &answer);
+					if(status != 0) {
+						mainGame->gMutex.lock();
+						mainGame->soundManager->PlaySoundEffect(SoundManager::SFX::INFO);
+						mainGame->addMessageBox(L"", dataManager.GetSysString(1412));
+						mainGame->gMutex.unlock();
+						break;
+					} else {
+						sockaddr_in * sin = ((struct sockaddr_in *)answer->ai_addr);
+						evutil_inet_ntop(AF_INET, &(sin->sin_addr), ip, 20);
+						remote_addr = htonl(inet_addr(ip));
+						evutil_freeaddrinfo(answer);
+					}
+				}
+				unsigned int remote_port = std::wcstol(portstr, nullptr, 10);
+				BufferIO::CopyWideString(pstr, mainGame->gameConf.lasthost);
+				BufferIO::CopyWideString(portstr, mainGame->gameConf.lastport);
+				if(DuelClient::StartClient(remote_addr, remote_port, false)) {
+					mainGame->btnCreateHost->setEnabled(false);
+					mainGame->btnJoinHost->setEnabled(false);
+					mainGame->btnJoinCancel->setEnabled(false);
+				}
 				#endif
 				break;
 			}
