8caa6c9ca2d0c18ff20389b014edd02538583def
fallenstardust <905309599@qq.com>
2026-02-04 12:41:00 +0800

    打字等操作其他悬浮窗app时失去焦点时不停止游戏

--- irrlicht/source/Irrlicht/Android/CIrrDeviceAndroid.cpp
+++ irrlicht/source/Irrlicht/Android/CIrrDeviceAndroid.cpp
@@ -280,7 +280,7 @@ void CIrrDeviceAndroid::handleAndroidCommand(ANDROID_APP app, int32_t cmd)
 		    break;
 		case APP_CMD_LOST_FOCUS:
 			os::Printer::log("Android command APP_CMD_LOST_FOCUS", ELL_DEBUG);
-			device->Focused = false;
+			//device->Focused = false;
 		break;
 		case APP_CMD_DESTROY:
 			os::Printer::log("Android command APP_CMD_DESTROY", ELL_DEBUG);
@@ -288,7 +288,7 @@ void CIrrDeviceAndroid::handleAndroidCommand(ANDROID_APP app, int32_t cmd)
 			break;
 		case APP_CMD_PAUSE:
 			os::Printer::log("Android command APP_CMD_PAUSE", ELL_DEBUG);
-			device->Paused = true;
+			//device->Paused = true;
 			break;
 		case APP_CMD_STOP:
 			os::Printer::log("Android command APP_CMD_STOP", ELL_DEBUG);
