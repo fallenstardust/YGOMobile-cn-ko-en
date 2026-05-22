5c6025526441f6c000a291568d279de6b8070608
fallenstardust <905309599@qq.com>
2026-05-22 12:34:42 +0800

    revert locking mutex in EGET_SCROLL_BAR_CHANGED (#3092)

--- Classes/gframe/event_handler.cpp
+++ Classes/gframe/event_handler.cpp
@@ -1050,7 +1050,6 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 			}
 			case SCROLL_CARD_SELECT: {
 				int pos = mainGame->scrCardList->getPos() / 10;
-				mainGame->gMutex.lock();
 				for(int i = 0; i < 5; ++i) {
 					// draw selectable_cards[i + pos] in btnCardSelect[i]
 					mainGame->stCardPos[i]->enableOverrideColor(false);
@@ -1117,12 +1116,10 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 							mainGame->stCardPos[i]->setBackgroundColor(0xff56649f);
 					}
 				}
-				mainGame->gMutex.unlock();
 				break;
 			}
 			case SCROLL_CARD_DISPLAY: {
 				int pos = mainGame->scrDisplayList->getPos() / 10;
-				mainGame->gMutex.lock();
 				for(int i = 0; i < 5; ++i) {
 					// draw display_cards[i + pos] in btnCardDisplay[i]
 					mainGame->stDisplayPos[i]->enableOverrideColor(false);
@@ -1166,7 +1163,6 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 							mainGame->stDisplayPos[i]->setBackgroundColor(0xff56649f);
 					}
 				}
-				mainGame->gMutex.unlock();
 				break;
 			}
 			break;
